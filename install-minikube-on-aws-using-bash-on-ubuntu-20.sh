53aaccc314532b4c967c8c75e55cd587e33d4b08
Richard Askew <richardaskew@yahoo.com>
2020-09-01 19:30:42 +0000

    removed package uninstall command

--- install-minikube-on-aws-using-bash-on-ubuntu-20.sh
+++ install-minikube-on-aws-using-bash-on-ubuntu-20.sh
@@ -20,7 +20,7 @@ echo "### Install Docker"
 echo "### https://docs.docker.com/engine/install/ubuntu/"
 echo "###"
 echo "###############"
-apt-get remove docker docker-engine docker.io containerd runc -y
+##apt-get remove docker docker-engine docker.io containerd runc -y
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
